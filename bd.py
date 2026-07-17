02a90f3245cbc2d64e8c6a3fb1ea7daf21665119
Mrbotrx <mrdarkrx41@gmail.com>
2026-07-17 12:28:13 +0600

    Update bd.py

--- bd.py
+++ bd.py
@@ -5,13 +5,16 @@ API = "https://www.btvlive.gov.bd/api/home"
 OUTPUT = "bdt.m3u8"
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0"
+    "User-Agent": "Mozilla/5.0",
+    "Referer": "https://www.btvlive.gov.bd/"
 }
 
 
 def create_playlist():
 
-    response = requests.get(
+    session = requests.Session()
+
+    response = session.get(
         API,
         headers=HEADERS,
         timeout=30
@@ -21,36 +24,72 @@ def create_playlist():
 
     data = response.json()
 
-    channels = data.get("channel_list", [])
+    # API cookie
+    cookies = session.cookies.get_dict()
+
+    cookie_string = "; ".join(
+        [f"{k}={v}" for k, v in cookies.items()]
+    )
+
+
+    channels = data.get(
+        "channel_list",
+        []
+    )
+
 
     with open(
         OUTPUT,
         "w",
         encoding="utf-8"
-    ) as file:
+    ) as f:
 
-        file.write("#EXTM3U\n")
+        f.write("#EXTM3U\n")
 
-        for channel in channels:
 
-            name = channel.get(
+        for ch in channels:
+
+            name = ch.get(
                 "channel_name",
                 "BTV"
             )
 
-            stream = channel.get(
+            logo = ch.get(
+                "poster",
+                ""
+            )
+
+            stream = ch.get(
                 "count_url",
                 ""
             )
 
+
             if ".m3u8" in stream:
 
-                file.write(
-                    f'#EXTINF:-1,{name}\n'
+
+                f.write(
+                    '#EXTINF:-1 '
+                    f'tvg-logo="{logo}" '
+                    f'tvg-name="{name}",'
+                    f'{name}\n'
                 )
 
-                file.write(
-                    stream + "\n"
+
+                # Cookie + Referer
+                if cookie_string:
+
+                    f.write(
+                        f'#EXTVLCOPT:http-referrer={HEADERS["Referer"]}\n'
+                    )
+
+                    f.write(
+                        f'#EXTVLCOPT:http-cookie={cookie_string}\n'
+                    )
+
+
+                f.write(
+                    stream + "\n\n"
                 )
 
 
